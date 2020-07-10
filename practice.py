b3b8308ca939d8a992888d974fc8a70a949115f2
JoongSeokD <ljseokd@gmail.com>
2020-07-10 17:22:50 +0900

    문자열 자료형

--- practice.py
+++ practice.py
@@ -1,3 +1,4 @@
+# 숫자 자료형
 print(5)
 print(-10)
 print(3.14)
@@ -5,3 +6,9 @@ print(1000)
 print(5+3)
 print(2*10)
 print(3*(3+12))
+
+#문자열 자료형
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+print("ㅋ"*9)
