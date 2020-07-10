d4d3b0b656ea3b0f3dfec4ebad473324ae700963
JoongSeokD <ljseokd@gmail.com>
2020-07-10 17:33:02 +0900

    변수

--- practice.py
+++ practice.py
@@ -21,3 +21,20 @@ print(False)
 print(not False)
 print(not True)
 print(not (5 > 10))
+
+#변수
+#애완동물을 소개해 주세요~
+#변수 사용 전
+print("우리집 강아지의 이름은 연탄입니다.")
+print("연탄이는 4살이며, 산책을 아주 좋아해요")
+print("연탄이는 어른일까요? True")
+#변수 사용 후 
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+print("우리집 " + animal + "의 이름은 " + name + "입니다.")
+# print(name + "는 " + str(age) + "살이며, " +  hobby +"을 아주 좋아해요")
+print(name , "는 " , age , "살이며, " , hobby ,"을 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
