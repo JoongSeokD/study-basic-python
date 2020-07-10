1763df60811f4656d45f915a60a2121d19fc0dda
JoongSeokD <ljseokd@gmail.com>
2020-07-10 17:34:38 +0900

    주석

--- practice.py
+++ practice.py
@@ -36,5 +36,11 @@ hobby = "산책"
 is_adult = age >= 3
 print("우리집 " + animal + "의 이름은 " + name + "입니다.")
 # print(name + "는 " + str(age) + "살이며, " +  hobby +"을 아주 좋아해요")
-print(name , "는 " , age , "살이며, " , hobby ,"을 아주 좋아해요")
+print(name , "는 " , age , "살이며, " , hobby ,"을 아주 좋아해요") # ,로 구분시 빈칸이 포함된다. ex) 연탄이 는  4 살이며,  산책 을 아주 좋아해요
 print(name + "는 어른일까요? " + str(is_adult))
+
+
+''' 이렇게 하면
+여러 문장이 
+주석처리 
+됩니다 '''
