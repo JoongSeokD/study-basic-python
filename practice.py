3bbf82b1f0f6d76cbae575d42f6c494ade42bd8c
JoongSeokD <ljseokd@gmail.com>
2020-07-11 23:31:56 +0900

    멤버변수

--- practice.py
+++ practice.py
@@ -773,3 +773,12 @@ class Unit:
 marine1 = Unit("마린", 40, 5)
 marine2 = Unit("마린", 40, 5)
 tank = Unit("탱크", 150, 35)
+
+#멤버 변수
+wraith1 = Unit("레이스", 80, 5)
+print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
+wraith2 = Unit("빼앗은 레이스", 80, 5)
+wraith2.clocking = True
+
+if wraith2.clocking == True:
+    print("{0}는 현재 클로킹 상태입니다.".format(wraith2.name))
