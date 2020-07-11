60c838b53ec2b5a01e42e08bcf89571faa24d00f
JoongSeokD <ljseokd@gmail.com>
2020-07-11 19:16:39 +0900

    튜플

--- practice.py
+++ practice.py
@@ -300,3 +300,17 @@ print(cabinet.items()) # dict_items([('B-100', '김태호'), ('C-20', '조세호
 
 #목욕탕 폐점
 cabinet.clear
+
+
+#튜플 
+menu = ("돈까스", "치즈까스")
+print(menu[0]) # 돈까스
+print(menu[1]) # 치즈까스
+
+#menu.add("생선까스") # AttributeError: 'tuple' object has no attribute 'add'
+name = "김종국"
+age = 20
+hobby = "코딩"
+print(name, age, hobby)
+(name, age, hobby) = ("김종국", 20, "코딩")
+print(name, age, hobby)
