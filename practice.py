f98005479c2c5d25bdd47b6436d5e5886d26b33e
JoongSeokD <ljseokd@gmail.com>
2020-07-11 22:08:53 +0900

    지역변수와 전역변수

--- practice.py
+++ practice.py
@@ -550,3 +550,23 @@ def profile_var(name, age, *language):
 
 profile_var("유재석",20, "자바", "파이썬", "C", "C++", "C#")
 profile_var("김태호",25, "코틀린", "스위프트")
+
+#지역변수와 전역변수
+gun = 10
+
+def checkpoint(soldiers):
+    global gun #전역공간에 잇는 gun 사용
+    gun = gun - soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+
+def checkpoint_ret(gun, soldiers):
+    gun = gun - soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+    return gun
+
+print("전체 총 : {0}".format(gun))
+checkpoint(2) #2명이 경계근무 나감
+print("남은 총 : {0}".format(gun))
+
+gun = checkpoint_ret(gun, 2)
+print("남은 총 : {0}".format(gun))
