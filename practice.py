a453d857cd851c89ce8f87841dbdd70280a6f1ee
JoongSeokD <ljseokd@gmail.com>
2020-07-11 17:37:28 +0900

    슬라이싱

--- practice.py
+++ practice.py
@@ -124,3 +124,15 @@ sentence3 = """
 하하
 """
 print(sentence3)
+
+#슬라이싱
+jumin = "950307-1234567"
+print("성별 : " + jumin[7])
+print("년도 : " + jumin[0:2]) # 0부터 2 직전까지 (0,1)
+print("월 : " + jumin[2:4])
+print("일 : " + jumin[4:6])
+print("생년월일 : " + jumin[0:6])
+print("생년월일 : " + jumin[:6]) # 처음부터 6직전까지
+print("뒤 7자리 : " + jumin[7:14])
+print("뒤 7자리 : " + jumin[7:]) # 7부터 끝까지
+print("뒤 7자리 (뒤에부터) : " + jumin[-7:])
