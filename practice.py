fdb58006e5997d072260c1656edf0629d085589c
JoongSeokD <ljseokd@gmail.com>
2020-07-12 00:03:28 +0900

    pass

--- practice.py
+++ practice.py
@@ -918,3 +918,19 @@ battlecruiser = FlyableAttackUnit2("배틀크루저", 500, 25, 3)
 
 vulture.move("11시")
 battlecruiser.move("9시")
+
+#pass
+class BuildingUnit(Unit4):
+    def __init__(self, name, hp, location):
+        pass
+
+supply_depot = BuildingUnit("서플라이 디폿", 500, "7시")
+
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다.")
+
+def game_over():
+    pass
+
+game_start()
+game_over()
