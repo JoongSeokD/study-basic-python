1d26ac1fab08ffb96a97d3803f261aa64a8ad9a6
JoongSeokD <ljseokd@gmail.com>
2020-07-11 23:38:32 +0900

    메소드

--- practice.py
+++ practice.py
@@ -725,7 +725,7 @@ with open("study.txt", "r", encoding="utf-8") as study_file:
 
 for i in range(1, 51):
     file_name = str(i) + "주차.txt"
-    with open(file_name), "w", encoding="utf-8") as report_file:
+    with open(file_name, "w", encoding="utf-8") as report_file:
         report_file.write("- {0} 주차 주간보고 - ".format(i))
         report_file.write("\n부서 : ")
         report_file.write("\n이름 : ")
@@ -782,3 +782,27 @@ wraith2.clocking = True
 
 if wraith2.clocking == True:
     print("{0}는 현재 클로킹 상태입니다.".format(wraith2.name))
+
+#메소드
+class AttackUnit:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+    
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]".format(self.name, location, self.damage))
+
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+
+
+firebat = AttackUnit("파이어뱃", 50, 16)
+firebat.attack("5시")
+firebat.damaged(25)
+firebat.damaged(25)
+
