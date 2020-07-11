e60282db9d11dfd17b2ac402ecf514f16901162a
JoongSeokD <ljseokd@gmail.com>
2020-07-11 17:18:15 +0900

    숫자처리함수

--- practice.py
+++ practice.py
@@ -89,3 +89,16 @@ number -= 2 # 16
 print(number)
 number %= 2 #0
 print(number)
+
+#숫자처리함수
+print(abs(-5)) # -5의 절대값 5
+print(pow(4,2)) # 4^2 16
+print(max(5,12)) # 최대값 반환 12
+print(min(5,12)) # 최소값 반환 5
+print(round(3.14)) #반올림 3
+print(round(4.99)) #반올림 5
+
+from math import *
+print(floor(4.99)) # 내림 4
+print(ceil(3.14)) #올림 4
+print(sqrt(16)) # 제곱근, 4
