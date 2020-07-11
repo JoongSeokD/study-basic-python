01942e2116af3cca54502a1a8683e7cc37ea43af
JoongSeokD <ljseokd@gmail.com>
2020-07-11 20:40:38 +0900

    50명의 승객과 매칭 기회가 있을 때, 총 탑승 승객 수를 구하는 프로그램

--- practice.py
+++ practice.py
@@ -459,4 +459,35 @@ print(students) # [8, 4, 10]
 #학생 이름을 대문자로 변환
 students = ["Iron man", "Thor", "I am groot"]
 students = [i.upper() for i in students]
-print(students)
+print(students) # ['IRON MAN', 'THOR', 'I AM GROOT']
+
+'''
+퀴즈) 당신은 Cocoa 서비스를 이용하는 택시 기사님입니다.
+50명의 승객과 매칭 기회가 있을 때, 총 탑승 승객 수를 구하는 프로그램을 작성하시오.
+
+조건1 : 승객별 운행 소요시간은 5 ~ 50분 사이의 난수로 정해집니다.
+조건2 : 당신은 소요 시간 5 ~ 15분 사이의 승객만 매칭해야 합니다.
+
+(출력문 예제)
+[O] 1번째 손님 (소요시간 : 15분)
+[ ] 2번째 손님 (소요시간 : 50분)
+[O] 3번째 손님 (소요시간 : 5분)
+...
+[ ] 50번째 손님 (소요시간 : 16분)
+
+총 탑승 승객 : 2명
+'''
+clients = [randint(5, 50) for i in range(1, 51)]
+count = 0
+check = ""
+index = 1
+for client in clients :
+    if client >= 5 and 15 >= client:
+        count += 1
+        check = "O"
+    else:
+        check = " "
+    print("[{0}] {1}번째 손님 (소요시간 : {2}분)".format(check, index, client))
+    index += 1
+print("총 탑승 승객 : {0}명".format(count))
+
