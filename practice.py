ed3ec0348ad97472f622a2056cc441b56616c58e
JoongSeokD <ljseokd@gmail.com>
2020-07-11 20:17:52 +0900

    while

--- practice.py
+++ practice.py
@@ -416,3 +416,19 @@ starbucks = ["아이언맨", "토르", "아이엠 그루트"]
 for customer in starbucks:
     print("{0}님 커피가 준비되었습니다.".format(customer))
 
+#while
+customer = "토르"
+index = 5
+while index >= 1:
+    print("{0}님 커피가 준비 되었습니다. {1}번 남았습니다.".format(customer, index))
+    index -= 1
+    if index == 0:
+        print("커피는 폐기 처분 되었습니다.")
+
+
+customer = "토르"
+person = "Unknown"
+
+while person != customer:
+    print("{0}, 커피가 준비되었습니다.".format(customer))
+    person = input("이름이 어떻게 되세요? ")
