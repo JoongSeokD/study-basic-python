6b88f324ea9498ab1426e4ddc4752d04b1a53d5f
JoongSeokD <ljseokd@gmail.com>
2020-07-11 17:59:59 +0900

    탈출문자

--- practice.py
+++ practice.py
@@ -174,3 +174,19 @@ print("나는 {age}살이며, {color}색을 좋아해요".format( color="빨간"
 age = 20
 color = "빨간"
 print(f"나는 {age}살이며, {color}색을 좋아해요")
+
+#탈출문자
+# \n : 줄바꿈
+print("백문이 불여일견 \n백견이 불여일타")
+print("저는 \"나도코딩\"입니다.") # 저는 "나도코딩"입니다.
+# \\ : 문장 내에서 \
+print("path : C:\\study\\study-basic-python") # path : C:\study\study-basic-python
+
+# \r : 커서를 맨 앞으로 이동(치환)
+print("Red Apple\rPine") # PineApple
+
+# \b : 백스페이스(한 글자 삭제)
+print("Redd\bApple") # RedApple
+
+# \t : 탭 (4칸이동)
+print("Red\tApple")
