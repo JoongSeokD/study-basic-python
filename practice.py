71f64879c3ad85949791ec2302e4970c185de963
JoongSeokD <ljseokd@gmail.com>
2020-07-11 23:51:07 +0900

    다중상속

--- practice.py
+++ practice.py
@@ -832,3 +832,41 @@ firebat = AttackUnit2("파이어뱃", 50, 16)
 firebat.attack("5시")
 firebat.damaged(25)
 firebat.damaged(25)
+
+#다중상속
+class Unit3:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+        
+
+class AttackUnit3(Unit3):
+    def __init__(self, name, hp, damage):
+        Unit3.__init__(self, name, hp)
+        self.damage = damage
+
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]".format(self.name, location, self.damage))
+
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+
+class Flyable:
+    def __init__(self, flying_spped):
+        self.flying_spped = Flyable
+
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]").format(name, location, self.flying_spped)
+
+
+ class FlyableAttackUnit(AttackUnit3, Flyable):
+     def __init__(self, name, hp, damage, flying_spped):
+         Unit3.__init__(self, name, hp)
+         Flyable.__init__(self, flying_spped)
+
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
