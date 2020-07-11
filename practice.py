ddcf93280d8e93fa4353c7fe4e970731f6edd311
JoongSeokD <ljseokd@gmail.com>
2020-07-11 18:17:49 +0900

    사이트별 비밀번호 생성기

--- practice.py
+++ practice.py
@@ -189,4 +189,21 @@ print("Red Apple\rPine") # PineApple
 print("Redd\bApple") # RedApple
 
 # \t : 탭 (4칸이동)
-print("Red\tApple")
+print("Red\tApple") # Red     Apple
+
+
+#퀴즈 사이트별로 비밀번호를 만들어 주는 프로그램을 작성하시오
+'''
+예 ) http://naver.com
+규칙1 : http:// 부분은 제외 => naver.com
+규칙2 : 처음 만나는 점(.) 이후 부분은 제외 => naver
+규칙3 : 남은 글자 중 처음 세자리 + 글자 갯수 + 글자 내 'e' 갯수 + "!"로 구성
+
+예) 생성된 비밀번호 : nav51!
+
+'''
+link = "http://naver.com"
+replaceLink = link.replace("http://", "") #규칙1
+domain = replaceLink[:replaceLink.index(".") ] #규칙2
+password = domain[:3] + str(len(domain)) + str(domain.count("e")) + "!" # 규칙 3
+print(f"{link}의 생성된 비밀번호는 {password} 입니다.")
