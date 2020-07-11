1170330a221354826513cf46b359f8e454916edf
JoongSeokD <ljseokd@gmail.com>
2020-07-11 20:52:20 +0900

    전달값과 반환값

--- practice.py
+++ practice.py
@@ -496,3 +496,27 @@ print("총 탑승 승객 : {0}명".format(count))
 def open_account():
     print("새로운 계좌가 생성되었습니다.")
 open_account()
+
+#전달값과 반환값
+def deposit(balance, money):
+    print("입금이 완료되었습니다. 잔액은 {0}원 입니다.".format(balance + money))
+    return balance + money
+
+def withdraw(balance, money):
+    if balance >= money: # 잔액이 출금보다 많으면
+        print("출금이 완료 되었습니다. 잔액은 {0}원입니다.".format(balance - money))
+        return balance - money
+    else:
+        print("출금이 실패 했습니다.")
+        return balance
+
+def withdraw_night(balance, money): # 저녁에 출금
+    commission = 100 #수수료 100원
+    return commission, balance - money - commission
+balance = 0
+balance = deposit(balance, 10000)
+balance = withdraw(balance, 2000)
+balance = withdraw(balance, 10000)
+commission, balance = withdraw_night(balance, 500)
+
+print("수수료는 {0}원이며, 잔액은 {1}원입니다.".format(commission, balance))
