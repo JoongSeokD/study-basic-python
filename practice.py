ff70b81a160d35a8322b398a90ada403bd726eb3
JoongSeokD <ljseokd@gmail.com>
2020-07-11 22:29:26 +0900

    표준입출력

--- practice.py
+++ practice.py
@@ -599,3 +599,25 @@ height = 175
 gender = "남자"
 weight = round(std_weight(height/100, gender),2)
 print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight))
+
+#표준입출력
+print("Python", "Java", sep=",", end="?")
+print("무엇이 더 재밌을까요?")
+
+import sys
+print("Python", "Java", file=sys.stdout) #표준 출력
+print("Python", "Java", file=sys.stderr) #표준 에러
+
+#시험 성적
+scores = {"수학":0, "영어":50, "코딩":100}
+for subject, score in scores.items():
+    print(subject.ljust(8), str(score).rjust(4), sep=":")
+
+# 은행 대기순번표
+# 001, 002, 003, ...
+for number in range(1, 21):
+    print("대기번호 : " + str(number).zfill(3)) 
+
+answer = input("아무 값이나 입력하세요 : ")
+print(type(answer)) #<class 'str'>
+print("입력하신 값은 " + answer + "입니다.")
