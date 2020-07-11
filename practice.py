eafc848c79fce6a8a767d1ed13544b565255e0f8
JoongSeokD <ljseokd@gmail.com>
2020-07-11 19:28:46 +0900

    세트

--- practice.py
+++ practice.py
@@ -314,3 +314,32 @@ hobby = "코딩"
 print(name, age, hobby)
 (name, age, hobby) = ("김종국", 20, "코딩")
 print(name, age, hobby)
+
+#집합(set)
+#중복 안됨, 순서 없음
+my_set = {1,2,3,3,3}
+print(my_set)
+
+java = {"유재석", "김태호", "양세형"}
+python = set(["유재석", "박명수"])
+
+# 교집합 (java와 python을 모두 할 수 있는 개발자)
+print(java & python) # {'유재석'}
+print(java.intersection(python)) # {'유재석'}
+
+#합집합 (java도 할수 있거나 python도 할 수 있는 개발자)
+print(java | python) # {'김태호', '박명수', '양세형', '유재석'}
+print(java.union(python)) # {'김태호', '박명수', '양세형', '유재석'}
+
+#차집합 (java 할 수 있찌만 python은 할줄 모르는 개발자)
+print(java - python) # {'양세형', '김태호'}
+print(java.difference(python)) # {'양세형', '김태호'}
+
+# python 할 줄 아는 사람이 늘어남
+python.add("김태호")
+python.add("김태호")
+print(python) # {'김태호', '유재석', '박명수'}
+
+# java를 잊었어요
+java.remove("김태호")
+print(python)
